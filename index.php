7162253dd51e426652badc47ced2b68f9db55755
xabolsz <abolfazl.c.r.7gaming@gmail.com>
2024-12-01 09:03:47 +0330

    Add files via upload

--- index.php
+++ index.php
@@ -30,17 +30,20 @@
             <DIV id="TABLIGH">
                 <p><h1>فروش</h1></p>
                 <p>(در اصطلاح حقوقی: بیع) به مبادلهٔ یک کالا یا خدمت با پول گفته می‌شود. شخصی که کالا یا خدمت را ارائه می‌دهد فروشنده و شخصی که هزینه را پرداخت می‌کند خریدار نامیده می‌شوند. فروش در بازرگانی فرآیند مبادله ارزش است که در آن فروشنده کالاها و خدمات خود را در ازای دریافت پول یا سایر دارایی ها ارائه می‌کند .عملیات فروش جز اساسی و تکمیل کننده یک فعالیت بازرگانی است[۱].</p>
-                <video src="ll.mp4"></video>
 
+
+                <video controls autoplay loop muted width="600">
+                    <source src="ll.mp4" type="video/mp4">{}
+                </video>
                 <h2>عوامل موثر در فروش</h2>
                 <ul>
                     <li>مهارت</li>
-                    <li><audio src=""></audio></li>
+                    <li><audio controls autoplay loop muted><source src="maharat.mp3" type="audio/mpeg"></audio>[]</li>
                     <li>ظاهر</li>
-                    <li><audio src=""></audio></li>
-                    <li>صحبت</li>
-                    <li><audio src=""></audio></li>
+                    <li><audio controls autoplay loop muted><source src="zaher.mp3" type="audio/mpeg"></audio>[]</li>
                     <li>زبان بدن</li>
+                    <li><audio controls autoplay loop muted><source src="zaban.mp3" type="audio/mpeg"></audio>[]</li>
+                    <li>صحبت</li>
                     <li><audio src=""></audio></li>
                 </ul>
             </DIV>
