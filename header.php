fb50fb7695af051b7f3ef3c477f059e7e489b853
xabolsz <abolfazl.c.r.7gaming@gmail.com>
2024-12-01 07:52:49 +0330

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+        <div class="photo1.png">
+            <a href="#">MR.befroosh</a>
+            <img src="photo1.png" alt="">
+        </div>
+        <nav>
+            
+            <ul>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="about.php">about</a></li>
+                <li><a href="">services</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+            <img src="photo2.jpg" alt="img">
+        </nav>
+    </header>
+    <aside><p></p></aside>
+    <section><ul><li><hr></li></ul></section>
+
+    <p><h1>سایت اقای فروشنده:</h1> این سایت برایه کمک به افراده تازه کار برایه راهاندازی کسب و کار خود و راحتر کردن فروشه محصواته خودساخته شده </p>
+    <section><ul><li><hr></li></ul></section>
---
+++ index.php
@@ -0,0 +1,59 @@
+
+<?php 
+  include("includes/header.php")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" type="text/css" href="site.css">
+
+    
+</head>
+<body>
+    
+    <MAIN>
+
+    
+        <section id="home">
+        <h1>welcome to  Mr.froshande site</h1>
+        </section>
+
+
+
+        <p><hr></p>
+
+            <DIV id="TABLIGH">
+                <p><h1>فروش</h1></p>
+                <p>(در اصطلاح حقوقی: بیع) به مبادلهٔ یک کالا یا خدمت با پول گفته می‌شود. شخصی که کالا یا خدمت را ارائه می‌دهد فروشنده و شخصی که هزینه را پرداخت می‌کند خریدار نامیده می‌شوند. فروش در بازرگانی فرآیند مبادله ارزش است که در آن فروشنده کالاها و خدمات خود را در ازای دریافت پول یا سایر دارایی ها ارائه می‌کند .عملیات فروش جز اساسی و تکمیل کننده یک فعالیت بازرگانی است[۱].</p>
+                <video src="ll.mp4"></video>
+
+                <h2>عوامل موثر در فروش</h2>
+                <ul>
+                    <li>مهارت</li>
+                    <li><audio src=""></audio></li>
+                    <li>ظاهر</li>
+                    <li><audio src=""></audio></li>
+                    <li>صحبت</li>
+                    <li><audio src=""></audio></li>
+                    <li>زبان بدن</li>
+                    <li><audio src=""></audio></li>
+                </ul>
+            </DIV>
+        
+
+
+    </MAIN>
+
+
+    
+</body>
+</html>
+
+<?php 
+  include("includes/footer.php")
+?>
